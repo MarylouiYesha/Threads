92f31f2f38ad0d896f1ac1fca5dae983b486ae3b
MarylouiYesha <yesha.maryloui@gmail.com>
2022-12-17 22:21:25 +0800

    class view

--- threads.py
+++ threads.py
@@ -130,6 +130,47 @@ class Consumer(Worker):
             self.buffer.task_done()
             self.simulate_idle()
 
+#class view
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
+
+    def animate(self):
+        with Live(self.render(), screen=True, refresh_per_second=10) as live:
+            while True:
+                live.update(self.render())
+
+    def render(self):
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue)
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue))
+            case _:
+                title = products = ""
+
+        rows = [Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+        
+    def panel(self, worker, title):
+        if worker is None:
+            return ""
+        padding = " " * int(29 / 100 * worker.progress)
+        align = Align(padding + worker.state, align="left", vertical="middle")
+        return Panel(align, height=5, title=title)
+
 #thread
 class Worker(threading.Thread):
       
